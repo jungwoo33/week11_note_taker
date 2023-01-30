5248d4cb480fd37ef414bff8de59cbcafbd3623e
Jungwoo Lee <jungwoo33@gmail.com>
2023-01-30 13:11:53 -0600

    port updated

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const db = require('./db/db.json');
 const uuid = require('./helpers/uuid');
 const path = require('path');
